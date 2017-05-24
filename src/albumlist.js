b6e190a622941178d674d505fcbf035822f8dbe4
manojkumar5251 <manojkumar5251@gmail.com>
2017-05-24 21:41:06 +0530

    Update albumlist.js

--- src/albumlist.js
+++ src/albumlist.js
@@ -9,7 +9,15 @@ class Albumlist extends Component {
   componentWillMount(){
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
     .then(hai => this.setState({albums:hai.data}))
-    .catch(() => alert("SORRRYYYYYY \n\t RELOAD"));
+    .catch(() => Alert.alert( 
+      'Alert Title', 
+      'My Alert Msg', 
+      [ 
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')}, 
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'}, 
+        {text: 'OK', onPress: () => console.log('OK Pressed')} 
+      ] 
+    ));
   }
 
 renderAlbums(){
